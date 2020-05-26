e15d696e049c479c8576f90bf7fdd3fc87816a61
Aline Goulart <alinegs.91@live.com>
2020-05-26 18:32:59 -0300

    backend

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes); 
 
-app.listen(3333); //define a porta que o acessa a aplicação
+app.listen(process.env.PORT || 3333); //define a porta que o acessa a aplicação
