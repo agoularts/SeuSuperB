f68aa64eda207bd0826cba56f1e3f880cdd8cfb8
Aline Goulart <alinegs.91@live.com>
2020-06-14 14:16:16 -0300

    controllers config

--- src/database/migrations/20200503160005_create_market.js
+++ src/database/migrations/20200503160005_create_market.js
@@ -1,20 +1,18 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('market', function (table){
-        table.string('cnpj').primary;
-        table.string('name').notNullable();
-        table.string('address').notNullable();
-        table.string('phone').notNullable();
-        table.string('city').notNullable();
-        table.string('uf').notNullable();
-        table.string('services').notNullable();
-/*
-        table.string('user_id').notNullable();
-
-        table.foreign('user_id').references('id').inTable('user');*/
-    });
+  return knex.schema.createTable('market', function (table){
+      table.integer('cnpj').primary;
+      table.string('name').notNullable();
+      table.string('address').notNullable();
+      table.double('latitude').notNullable();
+      table.double('longitude').notNullable();
+      table.integer('phone').notNullable();
+      table.string('city').notNullable();
+      table.string('uf').notNullable();
+      table.string('services').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('market');
+return knex.schema.dropTable('market');
 };
